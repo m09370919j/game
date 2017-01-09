ef91f76953cf695dfe2855f9affe49a87cf74a29
m09370919j <makan.j2000@gmail.com>
2017-01-09 21:23:50 +0330

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,10 +1,10 @@
 var config = {
-  "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+  "token": "211865746:AAExVhEZeV3CqJIZuLv7L13F1ptmx4NS87E",
   "update_type": "long-polling", // or "webhook"
   "webhook": "https://www.example.com/<token>",
   //"proxy": "http://127.0.0.1:8118",
   //"bot_name": "example_bot", // check bot name,
-  "admin_id": "35197423",
+  "admin_id": "90285047",
   "db": {
     host: '127.0.0.1',
     port: 27017,
